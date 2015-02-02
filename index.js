2d64aa5a156086930cbe77eaf859da3f43cac3ed
Rui Araújo <ruka.araujo@gmail.com>
2015-02-02 12:35:56 +0100

    Add missing libraries.

--- index.js
+++ index.js
@@ -4,31 +4,133 @@ var data = module.exports = {
   jquery: {
     versions: ['2.1.3', '2.1.2', '2.1.1', '2.1.0', '2.0.3', '2.0.2', '2.0.1', '2.0.0', '1.11.1', '1.11.0', '1.10.2', '1.10.1', '1.10.0', '1.9.1', '1.9.0', '1.8.3', '1.8.2', '1.8.1', '1.8.0', '1.7.2', '1.7.1', '1.7.0', '1.6.4', '1.6.3', '1.6.2', '1.6.1', '1.6.0', '1.5.2', '1.5.1', '1.5.0', '1.4.4', '1.4.3', '1.4.2', '1.4.1', '1.4.0', '1.3.2', '1.3.1', '1.3.0', '1.2.6', '1.2.3'],
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/jquery/' + version + '/jquery.min.js';
+      return 'https://ajax.googleapis.com/ajax/libs/jquery/' + version + '/jquery.min.js';
     }
   },
   'jquery-ui': {
     versions: ['1.11.2', '1.11.1', '1.11.0', '1.10.4', '1.10.3', '1.10.2', '1.10.1', '1.10.0', '1.9.2', '1.9.1', '1.9.0', '1.8.24', '1.8.23', '1.8.22', '1.8.21', '1.8.20', '1.8.19', '1.8.18', '1.8.17', '1.8.16', '1.8.15', '1.8.14', '1.8.13', '1.8.12', '1.8.11', '1.8.10', '1.8.9', '1.8.8', '1.8.7', '1.8.6', '1.8.5', '1.8.4', '1.8.2', '1.8.1', '1.8.0', '1.7.3', '1.7.2', '1.7.1', '1.7.0', '1.6.0', '1.5.3', '1.5.2'],
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/jqueryui/' + version + '/jquery-ui.min.js';
+      return 'https://ajax.googleapis.com/ajax/libs/jqueryui/' + version + '/jquery-ui.min.js';
     }
   },
   dojo: {
     versions: ['1.10.4', '1.10.3', '1.10.2', '1.10.1', '1.10.0', '1.9.3', '1.9.2', '1.9.1', '1.9.0', '1.8.5', '1.8.4', '1.8.3', '1.8.2', '1.8.1', '1.8.0', '1.7.5', '1.7.4', '1.7.3', '1.7.2', '1.7.1', '1.7.0', '1.6.1', '1.6.0', '1.5.2', '1.5.1', '1.5.0', '1.4.4', '1.4.3', '1.4.1', '1.4.0', '1.3.2', '1.3.1', '1.3.0', '1.2.3', '1.2.0', '1.1.1'],
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/dojo/' + version + '/dojo/dojo.js';
+      return 'https://ajax.googleapis.com/ajax/libs/dojo/' + version + '/dojo/dojo.js';
     }
   },
   swfobject: {
     versions: ['2.2', '2.1'],
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/swfobject/' + version + '/swfobject.js';
+      return 'https://ajax.googleapis.com/ajax/libs/swfobject/' + version + '/swfobject.js';
     }
   },
   'angular-material': {
     versions: ['0.8.3', '0.8.2', '0.8.1', '0.8.0', '0.7.1', '0.7.0', '0.6.1'],
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/angular_material/' + version + '/angular-material.min.js';
+      return 'https://ajax.googleapis.com/ajax/libs/angular_material/' + version + '/angular-material.min.js';
+    }
+  },
+  'angular-facebook': {
+    versions: ['0.2.3'],
+    url: function (version) {
+      return 'https://cdn.rawgit.com/Ciul/angular-facebook/v' + version + '/lib/angular-facebook.js';
+    }
+  },
+  'angular-google-plus': {
+    versions: ['0.1.2'],
+    url: function (version) {
+      return 'https://cdn.rawgit.com/mrzmyr/angular-google-plus/v' + version + '/dist/angular-google-plus.min.js';
+    }
+  },
+  'angular-google-chart': {
+    versions: ['0.0.11'],
+    url: function (version) {
+      return 'https://cdn.rawgit.com/bouil/angular-google-chart/' + version + '/ng-google-chart.js';
+    }
+  },
+  'ng-file-upload': {
+    versions: ['2.2.2'],
+    url: function (version) {
+      return 'https://cdn.rawgit.com/danialfarid/angular-file-upload/' + version + '/dist/angular-file-upload.min.js';
+    }
+  },
+  'angular-http-auth': {
+    versions: ['1.2.2'],
+    url: function (version) {
+      return 'https://cdn.rawgit.com/witoldsz/angular-http-auth/v' + version + '/src/http-auth-interceptor.js';
+    }
+  },
+  'ng-ckeditor': {
+    versions: ['0.2.1'],
+    url: function (version) {
+      return 'https://cdn.rawgit.com/esvit/ng-ckeditor/v' + version + '/ng-ckeditor.min.js';
+    }
+  },
+  'angular-underscore-module': {
+    versions: ['1.0.1'],
+    url: function (version) {
+      return 'https://cdn.rawgit.com/andresesfm/angular-underscore-module/v' + version + '/angular-underscore-module.js';
+    }
+  },
+  'angular-ui-router': {
+    versions: ['0.2.13'],
+    url: function (version) {
+      return 'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/' + version + '/angular-ui-router.min.js';
+    }
+  },
+  'lodash': {
+    versions: ['2.4.1'],
+    url: function (version) {
+      return 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/' + version + '/lodash.compat.min.js';
+    }
+  },
+  'underscore.string': {
+    versions: ['3.0.2', '2.4.0'],
+    url: function (version) {
+      return 'https://cdnjs.cloudflare.com/ajax/libs/underscore.string/' + version + '/underscore.string.min.js';
+    }
+  },
+  'angular-translate': {
+    versions: ['2.6.0'],
+    url: function (version) {
+      return 'https://cdnjs.cloudflare.com/ajax/libs/bower-angular-translate/' + version + '/angular-translate.min.js';
+    }
+  },
+  'angular-translate-loader-partial': {
+    versions: ['2.6.0'],
+    url: function (version) {
+      return 'https://cdn.rawgit.com/angular-translate/bower-angular-translate-loader-partial/' + version + '/angular-translate-loader-partial.min.js';
+    }
+  },
+  'angular-translate-storage-cookie': {
+    versions: ['2.6.0'],
+    url: function (version) {
+      return 'https://cdn.rawgit.com/angular-translate/bower-angular-translate-storage-cookie/' + version + '/angular-translate-storage-cookie.min.js';
+    }
+  },
+  'angular-translate-storage-local': {
+    versions: ['2.6.0'],
+    url: function (version) {
+      return 'https://cdn.rawgit.com/angular-translate/bower-angular-translate-storage-local/' + version + '/angular-translate-storage-local.min.js';
+    }
+  },
+  'ckeditor': {
+    versions: ['4.4.5'],
+    url: function (version) {
+      return 'https://cdnjs.cloudflare.com/ajax/libs/ckeditor/' + version + '/ckeditor.js';
+    }
+  },
+  'angular-data': {
+    versions: ['1.5.3'],
+    url: function (version) {
+      return 'https://cdnjs.cloudflare.com/ajax/libs/angular-data/' + version + '/angular-data.min.js';
+    }
+  },
+  'angular-local-storage': {
+    versions: ['0.1.5'],
+    url: function (version) {
+      return 'https://cdnjs.cloudflare.com/ajax/libs/angular-local-storage/' + version + '/angular-local-storage.min.js';
     }
   }
 };
@@ -54,7 +156,7 @@ angularFiles.forEach(function (item) {
   data[item] = {
     versions: [].concat(stableVersions).concat(unstableVersions),
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/angularjs/' + version + '/' + item + '.min.js';
+      return 'https://ajax.googleapis.com/ajax/libs/angularjs/' + version + '/' + item + '.min.js';
     }
   };
 });
